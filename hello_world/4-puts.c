142486e29e1a2ecb540e4ad9fb9f21978545346d
Ishay <6905@holbertonstudents.com>
2023-06-18 20:23:04 -0700

    Hello, puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Hello, puts
+ *return: the puts message
+ */
+int main(void)
+{
+	/**
+	 *return: 0 means ok
+	 */
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
