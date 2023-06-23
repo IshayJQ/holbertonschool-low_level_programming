a5801d52dcc6994b11dc83b3abefe7cdfa71ccad
Ishay <6905@holbertonstudents.com>
2023-06-22 18:56:03 -0700

    numbers of base 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program numbers of base 10
+ *
+ * Return: 0 on successful execution
+ */
+
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		printf("%d", number);
+		number++;
+	}
+	printf("\n");
+}
