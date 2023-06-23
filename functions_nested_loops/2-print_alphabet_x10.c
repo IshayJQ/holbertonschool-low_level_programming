ccba10e218264e0ca10c4d79326f9f9ab7b3aa1c
Ishay <6905@holbertonstudents.com>
2023-06-23 12:18:35 -0700

    rints 10 times the alphabet, in lowercase,

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet, in lowercase ten times
+ *
+ * Return: 0 on successful execution
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int t = 1;
+
+	while (t <= 10)
+	{
+		for (ch = 'a' ; ch <= 'z' ; ch++)
+		{
+			_putchar(ch);
+		}
+		t++;
+	}
+	_putchar('\n');
+}
