40021fd918226c25f4b0f75cac06bac7aa711737
Ishay <6905@holbertonstudents.com>
2023-06-23 14:09:03 -0700

    prints the sign of a number

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign- checks for lowercase character
+ * @n: is the number character to check
+ * Return: 1 is greater than zero or 0 is zero or -1 is less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
