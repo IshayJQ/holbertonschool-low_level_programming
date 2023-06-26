5edea67df5c634588203b94e0ffb386081be7b09
Ishay <6905@holbertonstudents.com>
2023-06-25 18:12:09 -0700

    multiplication table up to 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,16 +7,29 @@
 
 void times_table(void)
 {
-	char a, b, result;
+	char a, b, result, u, d;
 
 	for (a = 0 ; a < 10 ; a++)
 	{
 		for (b = 0 ; b < 10 ; b++)
 		{
 			result = a * b;
-			_putchar(result);
-			_putchar(39);
-			_putchar(32);
+			if (resul > 9)
+			{
+				u = result % 10;
+				d = (result - u) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + 48);
+				_putchar(u + 48);
+			}
+			else if (b != 0)
+			{
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+				_putchar(result + 48);
+			}
 		}
 		_putchar('\n');
 	}
