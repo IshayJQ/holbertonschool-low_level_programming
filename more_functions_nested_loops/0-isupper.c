520d2dda5c5ce083ccafc01b3824e5f0c7baec33
Ishay <6905@holbertonstudents.com>
2023-06-27 16:17:35 -0700

    checks for uppercase character

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _isalpha - checks for uppercase character
+ *  _isupper- checks for uppercase character
  * @c: is the number character to check
  * Return: 1 if is uppercase or 0 otherwise
  */
 
-int _isalpha(int c)
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
