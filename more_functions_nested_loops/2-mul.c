a7968365b240a49040b89ee282f755b29bbbbc7b
Ishay <6905@holbertonstudents.com>
2023-06-27 18:04:01 -0700

    multiplies two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - checks for lowercase character
+ * @a: first input number
+ * @b: second input number
+ * Return: the result multiplies two integers
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
