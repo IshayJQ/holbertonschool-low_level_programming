82d2dd29d1743fee731a569c5d3ea5292e0b92e5
Ishay <6905@holbertonstudents.com>
2023-06-28 13:42:45 -0700

    number of times

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -10,10 +10,16 @@ void print_line(int n)
 	int a;
 
 	if (n < 0)
+	{
 		_putchar('\n');
+		_putchar('\n');
+	}
 	else
 	{
 		for (a = 1 ; a <= n ; a++)
+		{
 			_putchar(95);
+			_putchar('\n');
+		}
 	}
 }
