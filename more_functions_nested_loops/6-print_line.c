b2729e39656fd5d66782791f5e4c9660f9b39a0c
Ishay <6905@holbertonstudents.com>
2023-06-28 13:40:00 -0700

    number of times

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - prints 10 times the numbers, from 0 to 14
+ * @n: is the number of times the character _ should be printed
+ * Return: not return
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	if (n < 0)
+		_putchar('\n');
+	else
+	{
+		for (a = 1 ; a <= n ; a++)
+			_putchar(95);
+	}
+}
