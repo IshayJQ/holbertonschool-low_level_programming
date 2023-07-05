a0ec7e8f479be2b8593843e5c89d926ac51e7064
Ishay <6905@holbertonstudents.com>
2023-07-05 14:00:33 -0700

    prints n elements of an array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+/**
+ * print_array - prints n elements of an array
+ * @a:string pointer to integer
+ * @n: lenght of the array
+ * Return: not return
+ */
+
+void print_array(int *a, int n)
+{
+	int cont = 1;
+
+	while (cont <= n)
+	{
+		if (cont == 1)
+			printf("%d", *a);
+		else
+			printf(", %d", *a);
+		if (cont != n)
+		a++;
+		cont++;
+	}
+}
