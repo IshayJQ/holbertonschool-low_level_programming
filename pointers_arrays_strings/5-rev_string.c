2ba21c9b589d76c5a080713c705d03b8b7e0e1f4
Ishay <6905@holbertonstudents.com>
2023-07-04 19:59:25 -0700

    reverses a string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: string pointer to char
+ * Return: not return
+ */
+
+void rev_string(char *s)
+{
+	int length, box;
+
+	for (length = 1 ; *s != '\0' ; s++)
+		length++;
+	char temp[length];
+	s--;
+	for (box = 0 ; box < length-1 ;box++)
+	{
+		temp[box] = *s;
+		printf("%c", temp[box]);
+		s--;
+	}
+	s++;
+	if (box == length-1)
+		temp[box] = '\0';
+	printf("\n");
+	printf("%s", temp);
+	for (box = 0 ; *s != '\0' ; box++)
+	{
+		*s = temp[box];
+		s++;
+	}
+}
