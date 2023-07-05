6024e9fc940af5c592d224b7efb50d5be2ab23d6
Ishay <6905@holbertonstudents.com>
2023-07-05 15:07:07 -0700

    copies the string pointed to

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,12 +9,25 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	while (*src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = *src;
-	return (dest);
+int length = 0;
+        int cont = 0;
+        char *p = dest;
+        
+        while (*src != '\0')
+        {
+                *dest = *src;
+                dest++;
+                src++;
+                length++;
+        }
+        /*while(cont <= length)
+        {
+            dest--;
+            cont++;
+        }*/
+        dest = p;
+        //*dest = dest[0];
+        printf("%s", dest);
+        printf("%c", dest[0]);
+        return(dest);
 }
