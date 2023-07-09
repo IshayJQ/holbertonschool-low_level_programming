8c3b060c2d79cffe10238c3bc62ae57ddc1662b5
Ishay <6905@holbertonstudents.com>
2023-07-09 16:20:59 -0700

    compares two strings

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: pointer to character
+ * @s2: pointer to character
+ * Return: number if is equal
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int length1, length2;
+
+	for (length1 = 0 ; s1[length1] != '\0' ; length1++)
+	{
+	}
+	for (length2 = 0 ; s2[length2] != '\0' ; length2++)
+	{
+	}
+	return (length1 - length2);
+}
