b246e717d6ef23615f82f237f8da09ccffb61055
Ishay <6905@holbertonstudents.com>
2023-07-09 21:08:33 -0700

    encodes a string into 1337

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - encodes a string into 1337
+ * @str: pointer to character
+ * Return: pointer to char whit change
+ */
+
+char *leet(char *str)
+{
+	char *p = str;
+	char reference[] = {'A', 'E', 'O', 'T', 'L'};
+	int change[] = {4, 3, 0, 7, 1};
+	int i;
+
+	while (*str != '\0')
+	{
+		for (i = 0; i < sizeof(reference) / sizeof(char); i++)
+		{
+			if (*str == reference[i] || *str == reference[i] + 32)
+				*str = 48 + change[i];
+		}
+		str++;
+	}
+	return (p);
+}
