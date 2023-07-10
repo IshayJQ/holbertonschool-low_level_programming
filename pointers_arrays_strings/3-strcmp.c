9caa894213eb44a7897b6964a749d8572869d518
Ishay <6905@holbertonstudents.com>
2023-07-09 17:01:49 -0700

    compares two strings

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -4,17 +4,14 @@
  * _strcmp - compares two strings
  * @s1: pointer to character
  * @s2: pointer to character
- * Return: number if is equal
+ * Return: if the character are differents return the subtraction number ASCII
  */
 int _strcmp(char *s1, char *s2)
 {
-	int length1, length2;
-
-	for (length1 = 0 ; s1[length1] != '\0' ; length1++)
+	while (*str1 == *str2 && *str1 != '\0' && *str2 != '\0')
 	{
+		str1++;
+		str2++;
 	}
-	for (length2 = 0 ; s2[length2] != '\0' ; length2++)
-	{
-	}
-	return (length1 - length2);
+	return *str1 - *str2;
 }
