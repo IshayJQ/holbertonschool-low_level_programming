6fe58a314dd1d704f0c19789cdf839cbfd490226
Ishay <6905@holbertonstudents.com>
2023-07-16 10:02:16 -0700

    prints its name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -8,11 +8,11 @@
  * Return: 0 always mean success
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
-  	return 0;
+	return (0);
 }
