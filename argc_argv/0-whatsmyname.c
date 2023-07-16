25711e221d2dfa81eee4f8c80fc16cc9103cbff8
Ishay <6905@holbertonstudents.com>
2023-07-16 09:59:25 -0700

    prints its name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,1 +1,18 @@
+#include "main.h"
+#include <stdio.h>
 
+/**
+ * main - prints its name
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 always mean success
+ */
+
+int main (int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+  	return 0;
+}
