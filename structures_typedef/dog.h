b93ca82b0fece00e6c5c70b2b4d547dfc6312d31
Ishay <6905@holbertonstudents.com>
2023-07-21 10:06:38 -0700

    Define a new type struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,1 +1,14 @@
+/**
+ * struct dog - data pet
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner for the dog
+ * Description: get the sigle data of a pet
+ */
 
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
