ddcb2582cc99cbc64063811647d25c91b110d989
Ishay <6905@holbertonstudents.com>
2023-08-05 16:58:37 -0700

    prints all the elements of list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list -  print the string of each node
+ * @h: pointer to struct list_t const
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t length = 0;
+	int i;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			printf("[%u] ", h->len);
+			for (i = 0; i < h->len; i++)
+				printf("%c", h->str[i]);
+		}
+		else
+			printf("[%d] (nil)", 0);
+		printf("\n");
+		h = h->next;
+		length++;
+	}
+	return (length);
+}
