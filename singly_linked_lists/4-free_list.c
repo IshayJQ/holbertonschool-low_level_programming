f7635a229f978f1bf184adea5ccd4479a66e5032
Ishay <6905@holbertonstudents.com>
2023-08-08 11:42:53 -0700

    free a lists

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * _free_list -  frees a lists
+ * @head: addres pointer to the head list struct list_t
+ * Return: the addres to the head list
+ */
+
+void _free_list(list_t *head)
+{
+	list_t *freeNext;
+
+	while (head->next != NULL)
+	{
+		freeNext = head->next;
+		free(head->str);
+		free(head);
+		head = freeNext;
+	}
+	if (head->next == NULL)
+	{
+		free(head->str);
+		free(head);
+	}
+}
