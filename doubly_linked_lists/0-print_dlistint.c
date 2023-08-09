c2fade7d850aed04829bd69eb4d9b5032be4cfef
Ishay <6905@holbertonstudents.com>
2023-08-09 08:57:23 -0700

    prints all the elements

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint -  print the int of each list
+ * @h: pointer to struct dlistint_t const
+ * Return: the number of lists
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t length = 0;
+	unsigned int i;
+
+	while (h != NULL)
+	{
+		printf("%d", h->n);
+		printf("\n");
+		length++;
+		h = h->next;
+	}
+	return (length);
+}
