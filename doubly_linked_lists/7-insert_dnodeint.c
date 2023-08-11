6035799661da8859274e5c33f0b5f3fb829bfb75
Ishay <6905@holbertonstudents.com>
2023-08-11 10:46:57 -0700

    inserts a new node at a given position

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,9 +13,10 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *listNode, *newList, *lastNode;
+	dlistint_t *listNode, *newList, *lastNode, *size;
 	unsigned int cont;
 
+	size = *h;
 	listNode = *h;
 	lastNode = NULL;
 	newList = malloc(sizeof(dlistint_t));
@@ -25,31 +26,44 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		printf("Error\n");
 		return (NULL);
 	}
-	if (*h == NULL)
+	for (cont = 1 ; size->next != NULL; cont++)
+		size = size->next;
+	if (idx <= cont)
 	{
-		newList->n = n;
-		newList->next = NULL;
-		newList->prev = NULL;
-		*h = newList;
+		if (*h == NULL)
+		{
+			newList->n = n;
+			newList->next = NULL;
+			newList->prev = NULL;
+			*h = newList;
+		}
+		else
+		{
+			for (cont = 0; cont < idx; cont++)
+			{
+				if (cont == idx - 1)
+					lastNode = listNode;
+				if (listNode->next == NULL)
+				{
+					newList->n = n;
+					newList->prev = listNode;
+					newList->next = NULL;
+					lastNode->next = newList;
+					return (newList);
+				}
+				listNode = listNode->next;
+			}
+			newList->prev = listNode->prev;
+			newList->next = listNode;
+			newList->n = n;
+			listNode->prev = newList;
+			if (lastNode != NULL)
+				lastNode->next = newList;
+			else
+				*h = newList;
+			return (newList);
+		}
 	}
 	else
-	{
-		for (cont = 0; cont < idx; cont++)
-		{
-			if (cont == idx - 1)
-				lastNode = listNode;
-			listNode = listNode->next;
-			if (listNode == NULL)
-				return (NULL);
-		}
-		newList->prev = listNode->prev;
-		newList->next = listNode;
-		newList->n = n;
-		listNode->prev = newList;
-		if (lastNode != NULL)
-			lastNode->next = newList;
-		else
-			*h = newList;
-	}
-	return (newList);
+		return (NULL);
 }
