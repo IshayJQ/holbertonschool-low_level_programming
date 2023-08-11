d7fdfa1b78c8d33dcf2f284e30e270ea66b12bd6
Ishay <6905@holbertonstudents.com>
2023-08-10 20:00:14 -0700

    inserts a new node at a given position

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -4,37 +4,38 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index -  get node by the index
- * @head: addres pointer to the head list struct list_t
- * @index: the number node
+ * insert_dnodeint_at_index -  insert new node by the index
+ * @h: addres pointer to the head list struct list_t
+ * @idx: the number node
+ * @n: int number data
  * Return: the addres of the node
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *listNode, *newList, *lastNode;
-    unsigned int cont;
-    
-    listNode = *h;
-    newList = malloc(sizeof(dlistint_t));
-    
-    if (newList == NULL)
-    {
-        printf("Error\n");
-        return (NULL);
-    }
-    for (cont = 0; cont < idx; cont++)
-    {
-        if (listNode == NULL)
-            return (NULL);
-        if (cont == idx-1)
-          lastNode = listNode;
-        listNode = listNode->next;
-    }
-    newList->prev = listNode->prev;
-    newList->next = listNode;
-    newList->n = n;
-    listNode->prev = newList;
-    lastNode->next = newList;
-    return (newList);
+	dlistint_t *listNode, *newList, *lastNode;
+	unsigned int cont;
+
+	listNode = *h;
+	newList = malloc(sizeof(dlistint_t));
+	
+	if (newList == NULL)
+	{
+		printf("Error\n");
+		return (NULL);
+	}
+	for (cont = 0; cont < idx; cont++)
+	{
+		if (listNode == NULL)
+			return (NULL);
+		if (cont == idx-1)
+			lastNode = listNode;
+		listNode = listNode->next;
+	}
+	newList->prev = listNode->prev;
+	newList->next = listNode;
+	newList->n = n;
+	listNode->prev = newList;
+	lastNode->next = newList;
+	return (newList);
 }
