46fe5f8072c658cd61b4d9b5b1e36d235a2a6067
Ishay <6905@holbertonstudents.com>
2023-08-12 15:24:53 -0700

    new node at a given position.

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,18 +13,19 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *listNode = *h, *newList = NULL, *lastNode = NULL;
+	dlistint_t *listNode = *h, *newList = NULL, *lastNode = NULL, *size = *h;
 	unsigned int cont;
 	size_t size;
 	int res = 0;
 
-	size = dlistint_len(listNode);
-	res = idx - size;
+	for (cont = 1 ; size->next != NULL; cont++)
+		size = size->next;
+	res = idx - cont;
 	if (res >= 1 && *h != NULL)
 		newList = NULL;
 	else
 	{
-		if (*h == NULL || idx - size == 0)
+		if (*h == NULL || idx - cont == 0)
 		{
 			newList = add_dnodeint_end(&listNode, n);
 			return (newList);
