75aeb466e40cb0dd81fa2dda5ed7a7a7f9de7d51
Ishay <6905@holbertonstudents.com>
2023-08-12 15:09:32 -0700

    new node at a given position.

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,57 +13,38 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *listNode, *newList, *lastNode, *size;
+	dlistint_t *listNode = *h, *newList = NULL, *lastNode = NULL;
 	unsigned int cont;
+	size_t size;
+	int res = 0;
 
-	size = *h;
-	listNode = *h;
-	lastNode = NULL;
-	newList = malloc(sizeof(dlistint_t));
-
-	if (newList == NULL)
+	size = dlistint_len(listNode);
+	res = idx - size;
+	if (res >= 1 && *h != NULL)
+		newList = NULL;
+	else
 	{
-		printf("Error\n");
-		return (NULL);
-	}
-	for (cont = 1 ; size->next != NULL; cont++)
-		size = size->next;
-	if (idx <= cont)
-	{
-		if (*h == NULL)
+		if (*h == NULL || idx - size == 0)
 		{
-			newList->n = n;
-			newList->next = NULL;
-			newList->prev = NULL;
-			*h = newList;
+			newList = add_dnodeint_end(&listNode, n);
+			return (newList);
 		}
+		else if (idx == 0)
+			newList = add_dnodeint(&listNode, n);
 		else
 		{
 			for (cont = 0; cont < idx; cont++)
 			{
 				if (cont == idx - 1)
 					lastNode = listNode;
-				if (listNode->next == NULL)
-				{
-					newList->n = n;
-					newList->prev = listNode;
-					newList->next = NULL;
-					lastNode->next = newList;
-					return (newList);
-				}
 				listNode = listNode->next;
 			}
-			newList->prev = listNode->prev;
-			newList->next = listNode;
-			newList->n = n;
+			add_dnodeint(&newList, n);
 			listNode->prev = newList;
-			if (lastNode != NULL)
-				lastNode->next = newList;
-			else
-				*h = newList;
-			return (newList);
+			newList->prev = lastNode;
+			newList->next = listNode;
+			lastNode->next = newList;
 		}
 	}
-	else
-		return (NULL);
+	return (newList);
 }
